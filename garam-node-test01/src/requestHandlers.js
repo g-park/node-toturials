ff1b9d95c052be402f140850e121d7068a22d7f6
garamsoft <garamsoft@gmail.com>
2014-08-22 12:11:40 -0400

    IncommingForm 추

--- garam-node-test01/src/requestHandlers.js
+++ garam-node-test01/src/requestHandlers.js
@@ -27,7 +27,7 @@ function start(response, postData) {
 function upload(response, postData) {
 
   console.log("Request handler 'upload' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.writeHead(200, {"Content-Type": "text/plain","charset":"utf-8"});
   response.write("You've sent: " +  querystring.parse(postData).text);
   response.end();
 }
