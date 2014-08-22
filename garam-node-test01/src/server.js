76f0d2171d2d8710ed4157cdd96dac12210da63c
garamsoft <garamsoft@gmail.com>
2014-08-22 08:07:16 -0400

    Server.js 소스에 자세한 설명을 다른 주석이 첨부 되었습니다

--- garam-node-test01/src/server.js
+++ garam-node-test01/src/server.js
@@ -6,9 +6,16 @@ function start() {
 
   function onRequest(request, response) {
     console.log("Request received.");
+
+    //해더 설정
     response.writeHead(200, {"Content-Type": "text/plain"});
+    //바디부분을 설정
     response.write("Hello World");
+    //끝내기
     response.end();
+
+    /*만약에 요청에 대한 자세한 사항이 있다면 request 객체를 사용할 수 있다*/
+
   }
 
   //onReqeust함수의 인자로 두개가 들어오는데
